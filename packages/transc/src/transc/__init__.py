fd72260661254cf28bb01b679ad2fe407e2b27db
auggiemarignier <auggie.marignier@earth.ox.ac.uk>
2026-01-06 09:14:17 +0100

    feat: TransC
    Squashed commit of the following:
    
    commit f49c1b57f50ea0ec854a176437201b11f810c2e8
    Author: auggiemarignier <auggie.marignier@earth.ox.ac.uk>
    Date:   Tue Dec 23 14:45:25 2025 +0100
    
        feat: bloody thin TransC Wrapper

--- packages/transc/src/transc/__init__.py
+++ packages/transc/src/transc/__init__.py
@@ -1,1 +1,23 @@
 """TransConceptual sampling methods for estimating model evidence."""
+
+from pytransc.analysis.visits import get_relative_marginal_likelihoods as _grml
+from pytransc.analysis.visits import get_visits_to_states
+from pytransc.samplers import run_ensemble_resampler
+from pytransc.utils.types import FloatArray
+
+
+def get_relative_marginal_likelihoods(*args, walker_average="mean") -> FloatArray:
+    """Get relative marginal likelihoods of visited states.
+
+    This is a wrapper around the `get_relative_marginal_likelihoods` function
+    from the `pytransc.analysis.visits` module.
+    """
+    visits = get_visits_to_states(*args)
+    return _grml(visits[-1], walker_average=walker_average)
+
+
+__all__ = [
+    "run_ensemble_resampler",
+    "get_visits_to_states",
+    "get_relative_marginal_likelihoods",
+]
