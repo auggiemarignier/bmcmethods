870d51983377adb8a0e8795bb907ed4ad34f65a5
auggiemarignier <auggie.marignier@earth.ox.ac.uk>
2026-02-18 09:48:23 +0000

    test: refactor fixtures

--- packages/sampling/tests/test_sampling/test_priors/test_wrapped.py
+++ packages/sampling/tests/test_sampling/test_priors/test_wrapped.py
@@ -33,6 +33,9 @@ def test_valid_initialisation(
     assert prior.wrap_bounds == [(-180.0, 180.0)]
     assert prior.type == prior_type
     assert isinstance(prior._base, expected_class)
+    for attr in kwargs:
+        assert hasattr(prior._base, attr)
+        np.testing.assert_allclose(getattr(prior._base, attr), kwargs[attr])
 
 
 def test_invalid_prior_type() -> None:
@@ -42,68 +45,45 @@ def test_invalid_prior_type() -> None:
         WrappedPrior(wrap_bounds=[(-180.0, 180.0)], type="invalid_type")
 
 
-@pytest.mark.parametrize(
-    "prior_type,kwargs",
-    [
-        (
-            PriorType.GAUSSIAN,
-            {"mean": np.array([0.0]), "inv_covar": np.array([[1.0]])},
-        ),
+@pytest.fixture(
+    params=[
+        (PriorType.GAUSSIAN, {"mean": np.array([0.0]), "inv_covar": np.array([[1.0]])}),
         (
             PriorType.UNIFORM,
             {"lower_bounds": np.array([-180.0]), "upper_bounds": np.array([180.0])},
         ),
     ],
+    ids=["gaussian", "uniform"],
 )
-def test_model_parameter_wrapping(
-    prior_type: PriorType, kwargs: dict[str, Any]
-) -> None:
+def wrapped_prior(request) -> WrappedPrior:
+    prior_type, kwargs = request.param
+    return WrappedPrior(wrap_bounds=[(-180.0, 180.0)], type=prior_type, **kwargs)
+
+
+def test_model_parameter_wrapping(wrapped_prior: WrappedPrior) -> None:
     """Test that model parameters are correctly wrapped around the specified bounds."""
 
-    prior = WrappedPrior(wrap_bounds=[(-180.0, 180.0)], type=prior_type, **kwargs)
+    model_params_and_wrapped = [
+        (-181.0, 179.0),  # Just below lower bound should wrap to just below upper bound
+        (180.0, -180.0),  # Just above upper bound should wrap to just above lower bound
+    ]
+    for model_param, expected_wrapped in model_params_and_wrapped:
+        model_params = np.array([model_param])
+        wrapped_model_params = wrapped_prior._wrap(model_params)
+        expected_wrapped_model_params = np.array([expected_wrapped])
 
-    # Test wrapping for a parameter just outside the lower bound
-    model_params = np.array([-181.0])  # This should wrap to 179.0
-    wrapped_model_params = prior._wrap(model_params)
-    expected_model_params = np.array([179.0])
-    np.testing.assert_allclose(wrapped_model_params, expected_model_params)
-    assert wrapped_model_params.shape == model_params.shape
-
-    # Test wrapping for a parameter just outside the upper bound
-    model_params = np.array([181.0])  # This should wrap to -179.0
-    wrapped_model_params = prior._wrap(model_params)
-    expected_model_params = np.array([-179.0])
-    np.testing.assert_allclose(wrapped_model_params, expected_model_params)
-    assert wrapped_model_params.shape == model_params.shape
+        np.testing.assert_allclose(wrapped_model_params, expected_wrapped_model_params)
+        assert wrapped_model_params.shape == model_params.shape
 
 
-@pytest.mark.parametrize(
-    "prior_type,kwargs",
-    [
-        (
-            PriorType.GAUSSIAN,
-            {"mean": np.array([0.0]), "inv_covar": np.array([[1.0]])},
-        ),
-        (
-            PriorType.UNIFORM,
-            {"lower_bounds": np.array([-180.0]), "upper_bounds": np.array([180.0])},
-        ),
-    ],
-)
-def test_call(prior_type: PriorType, kwargs: dict[str, Any]) -> None:
+def test_call(wrapped_prior: WrappedPrior) -> None:
     """Test that calling the WrappedPrior returns the log-prior from the base prior, modulo wrapping."""
 
-    prior = WrappedPrior(
-        wrap_bounds=[(-180.0, 180.0)],
-        type=prior_type,
-        **kwargs,
-    )
-
     model_params = np.array([-181.0])  # This should wrap to 179.0
-    log_prior = prior(model_params)
+    log_prior = wrapped_prior(model_params)
 
     wrapped_params = np.array([179.0])
-    expected_log_prior = prior._base(
+    expected_log_prior = wrapped_prior._base(
         wrapped_params
     )  # Calculate expected log-prior using the base prior with wrapped parameters
 
