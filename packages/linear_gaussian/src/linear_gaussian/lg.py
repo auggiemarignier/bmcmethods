ec8574ec854fd39cabf4670cc948b235207519fe
auggiemarignier <auggie.marignier@earth.ox.ac.uk>
2026-09-02 14:48:36 +0100

    feat: `clear_cache`

--- packages/linear_gaussian/src/linear_gaussian/lg.py
+++ packages/linear_gaussian/src/linear_gaussian/lg.py
@@ -608,3 +608,9 @@ def _prepare_problem(
         _CACHE.pop(next(iter(_CACHE)))
     _CACHE[key] = prepared
     return prepared
+
+
+def clear_cache() -> None:
+    """Clear the prepared-problem cache and associated ID->token map."""
+    _CACHE.clear()
+    _ID_TOKEN_MAP.clear()
